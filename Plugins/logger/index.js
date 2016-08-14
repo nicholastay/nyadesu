e585d433eb89cc76e2d52eac93d3e3441b301fd8
Nicholas Tay <nexerq@gmail.com>
2016-08-14 16:57:12 +1000

    Improved logging stuff

--- Plugins/logger/index.js
+++ Plugins/logger/index.js
@@ -1,14 +1,30 @@
 "use strict";
 
+const eris = require("eris")
+    , chalk = require("chalk");
+
 const Plugin = require("../../Base/Plugin");
 
 class Logger extends Plugin {
     constructor() {
         super();
 
-        this.addRawHandler("consolelogger", m => {
-            Nyadesu.Logging.log("Client-Chat", `${m.author.username}: ${m.content}`);
-        });
+        this.addRawHandler("consolelogger", this.logToConsole.bind(this));
+    }
+
+    logToConsole(m) {
+        let c = m.content;
+        if (c.indexOf("\n") >= 0)
+            c = c.split("\n").shift() + "... (truncated new line)";
+
+        if (m.channel instanceof eris.PrivateChannel) {
+            if (m.author.id === Nyadesu.Client.user.id)
+                return Nyadesu.Logging.log("Client-PM", chalk.magenta("nyadesu") + " -> " + chalk.yellow(`${m.author.username}#${m.author.discriminator}`) + `: ${c}`);
+
+            return Nyadesu.Logging.log("Client-PM", chalk.yellow(`${m.author.username}#${m.author.discriminator}`) + " -> " + chalk.magenta("nyadesu") + `: ${c}`);
+        }
+
+        return Nyadesu.Logging.log("Client-Chat", chalk.magenta(`[${m.channel.guild.name} // #${m.channel.name}] `) + chalk.yellow(`${m.author.username}#${m.author.discriminator}`) + `: ${c}`);
     }
 }
 
