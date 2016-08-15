5d1c46cb6b28eaf6c9b13d28732f4631538cce22
Nicholas Tay <nexerq@gmail.com>
2016-08-15 21:24:58 +1000

    More eris patching convenience stuffs

--- Core/Client.js
+++ Core/Client.js
@@ -21,8 +21,25 @@ class Client extends Eris {
 
         // patch
         ErisMonkeypatch.patch(this);
+        otherErisPatches();
         Nyadesu.Logging.success("Client", "Loaded Eris client + slight OOP patched.");
+    }
+
+    replyMessage(message, content, file) {
+        this.createMessage(message.channel.id, `${message.author.mention} ${content}`, file);
+    }
+
+    softReplyMessage(message, content, file) {
+        this.createMessage(message.channel.id, `\`${message.author.softMention}\`: ${content}`, file);
     }
 }
 
 module.exports = Client;
+
+
+function otherErisPatches() {
+    // non generic patches
+
+    // user#discrim getterl
+    Object.defineProperty(Eris.User.prototype, "softMention", { get: function() { return `${this.username}#${this.discriminator}` } });
+}
