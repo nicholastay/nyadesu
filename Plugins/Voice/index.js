dc12543545fdea5c7958d8cab85bd3ad7c7e4b03
Nicholas Tay <nexerq@gmail.com>
2016-08-16 09:21:57 +1000

    WIP voice leaving in command

--- Plugins/Voice/index.js
+++ Plugins/Voice/index.js
@@ -12,16 +12,26 @@ class Voice extends Plugin {
         this.connections = {};
         this.addCommand(new PluginCommand("voice", {
             allowPM: false,
-            onReturnSuccess: true
-        }, this.joinVoiceCommand.bind(this)));
+            onReturnSuccess: true,
+            softReply: true
+        }, this.voiceCommand.bind(this)));
     }
 
-    joinVoiceCommand(tail, author, channel) {
-        if (!tail[0] && !author.voiceState)
-            throw new UserError("You must be in a voice channel or specify one for me to join...");
+    voiceCommand(tail, author, channel) {
+        if (this.connections[channel.guild.id]) {
+            if (this.connections[channel.guild.id].textChannel.id === channel.id) {
+                return Nyadesu.Client.leaveVoiceChannel(this.connections[channel.guild.id].voiceChannel.id)
+                    .then(() => "Successfully left the voice channel and unbound.");
+            } else {
+                throw new UserError(`There is already a voice connection in this server and the text channel is bound in ${this.connections[channel.guild.id].textChannel.mention}.`);
+            }
+        }
 
         let vc;
-        if (!tail[0] && author.voiceState) {
+        if (!tail[0]) {
+            if (!author.voiceState)
+                throw new UserError("You must be in a voice channel or specify one for me to join...");
+
             vc = channel.guild.channels.find(c => c.id === author.voiceState.channelID);
             if (!vc)
                 throw new UserError("The voice channel you are currently in is not in this server...");
@@ -33,7 +43,7 @@ class Voice extends Plugin {
 
         return Nyadesu.Client.joinVoiceChannel(vc.id)
             .then(conn => {
-                this.connections[vc.id] = new VoiceConnection(conn, vc, channel);
+                this.connections[channel.guild.id] = new VoiceConnection(conn, vc, channel);
                 return "Successfully joined the voice channel and bound to this text channel.";
             });
     }
