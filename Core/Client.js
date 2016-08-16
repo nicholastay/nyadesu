8f172b4377c775bb41d7f167c30d14cd8ed50b62
Nicholas Tay <nexerq@gmail.com>
2016-08-17 08:20:04 +1000

    softMention for members too

--- Core/Client.js
+++ Core/Client.js
@@ -41,6 +41,7 @@ function otherErisPatches() {
 
     // user#discrim getterl
     Object.defineProperty(Eris.User.prototype, "softMention", { get: function() { return `${this.username}#${this.discriminator}`; } });
+    Object.defineProperty(Eris.Member.prototype, "softMention", { get: function() { return `${this.username}#${this.discriminator}`; } });
 
     // prototypes for our reply/softReply
     Eris.Message.prototype.reply = function(content, file) { return Nyadesu.Client.replyMessage(this, content, file); };
