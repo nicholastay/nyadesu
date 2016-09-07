028cad4a3a9b266bc325764770ab662d1ee6f17a
Nicholas Tay <nexerq@gmail.com>
2016-09-07 20:26:15 +1000

    Blockify command

--- Plugins/Novelty/index.js
+++ Plugins/Novelty/index.js
@@ -4,7 +4,22 @@ const fetch = require("node-fetch");
 
 const Plugin = require("../../Base/Plugin")
     , PluginCommand = require("../../Base/PluginCommand")
-    , BucketInfo = require("../../Base/BucketInfo");
+    , BucketInfo = require("../../Base/BucketInfo")
+    , UserError = require("../../Base/UserError");
+
+const NON_ALPHANUMERIC_SPACE_CHECK = /[^A-Za-z0-9 ]/;
+const NUMBER_LITERAL_MAP = {
+    "0": "zero",
+    "1": "one",
+    "2": "two",
+    "3": "three",
+    "4": "four",
+    "5": "five",
+    "6": "six",
+    "7": "seven",
+    "8": "eight",
+    "9": "nine"
+};
 
 class Novelty extends Plugin {
     constructor() {
@@ -14,12 +29,37 @@ class Novelty extends Plugin {
             reply: true,
             rateLimitedInfo: new BucketInfo("Novelty.meow", 15, "minute", { perUser: true })
         }, this.meowCommand));
+
+        this.addCommand(new PluginCommand("blockify", {
+            reply: true,
+            requireInput: 1,
+        }, this.blockifyCommand));
     }
 
     meowCommand() {
         return fetch("http://random.cat/meow")
             .then(r => r.json())
             .then(j => j.file);
+    }
+
+    blockifyCommand(tail) {
+        let data = tail.join(" ");
+
+        if (NON_ALPHANUMERIC_SPACE_CHECK.test(data))
+            throw new UserError("Invalid input, you may only use letters, numbers and spaces.")
+
+        return data.split("").map(w => {
+            if (w === " ")
+                return " "; // return just a space. will be padded out when joined
+
+            // blockify stuff
+            // numbers handling emoji
+            if (!isNaN(w))
+                return `:${NUMBER_LITERAL_MAP[w]}:`;
+
+            // send emoji letters
+            return `:regional_indicator_${w.toLowerCase()}:`;
+        }).join(" "); // needs to be spaced out as unicode gets stuffed
     }
 }
 
