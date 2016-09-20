5bafdf3d794f26c06c77b1d31c19ab6aadbdd912
Nicholas Tay <nexerq@gmail.com>
2016-09-20 21:41:25 +1000

    Updatelog via git log

--- Plugins/Utility/index.js
+++ Plugins/Utility/index.js
@@ -1,7 +1,9 @@
 "use strict";
 
 const fetch = require("node-fetch")
-    , htmlToText = require("html-to-text");
+    , htmlToText = require("html-to-text")
+    , path = require("path")
+    , simpleGit = require("simple-git")(path.resolve(__dirname, "../../"));
 
 const Plugin = require("../../Base/Plugin")
     , BucketInfo = require("../../Base/BucketInfo")
@@ -9,6 +11,7 @@ const Plugin = require("../../Base/Plugin")
     , UserError = require("../../Base/UserError");
 
 const DOWNFOREVERYONE_REGEX = /just you\. (.*?) is up\./;
+simpleGit.logAsync = Promise.promisify(simpleGit.log);
 
 class Utility extends Plugin {
     constructor() {
@@ -19,6 +22,8 @@ class Utility extends Plugin {
             requireInput: 1,
             rateLimitedInfo: new BucketInfo("Core.testWebsite", 3, "minute", { perUser: true })
         }, this.testWebsiteCommand));
+
+        this.addCommand(new PluginCommand("nyaupdates", this.updatesCommand));
     }
 
     testWebsiteCommand(tail) {
@@ -36,6 +41,12 @@ class Utility extends Plugin {
 
                 return `✅ \`${reg[1]}\` seems to be up, online and functional.`;
             });
+    }
+
+    updatesCommand() {
+        return simpleGit.logAsync(["-n", "5"]) // last 5
+            .then(r => r.all.map(d => `     * \`${d.hash.substr(0, 7)}\` ${d.message}`))
+            .then(c => `**Updatelog - Last 5 commits made to** \`nyadesu\` (master)\n${c.join("\n")}`);
     }
 }
 
