96d762bbe9e1b7e7c812ac62edc92d5e2a62d53f
Nicholas Tay <nexerq@gmail.com>
2016-12-02 18:51:00 +1100

    osu! module embeds

--- Plugins/Osu/index.js
+++ Plugins/Osu/index.js
@@ -14,7 +14,9 @@ class Osu extends Plugin {
 
         this.addCommand(new PluginCommand("osu", {
             requireInput: 1,
-            rateLimitedInfo: new BucketInfo("Osu.getUser", 20, "minute", { perUser: true })
+            rateLimitedInfo: new BucketInfo("Osu.getUser", 20, "minute", { perUser: true }),
+            embed: true,
+            embedFooter: true
         }, this.osuCommand.bind(this)));
     }
 
@@ -27,20 +29,79 @@ class Osu extends Plugin {
                 let level = Math.floor(Number(data.level));
                 let percentInLevel = Math.round((data.level - level) * 100);
 
-                return `\`\`\`
-osu! profile information for: ${data.username}
----------------------------------------------------------
-Username: ${data.username}
-User ID: ${data.user_id}
-Avatar: https://a.ppy.sh/${data.user_id}.jpg
-PP: ${Math.floor(Number(data.pp_raw))}pp
-Rank: #${data.pp_rank} (#${data.pp_country_rank} in ${data.country})
-Accuracy: ${Number(data.accuracy).toFixed(2)}%
-Level: ${level} (${percentInLevel}%)
-300/100/50: ${data.count300}/${data.count100}/${data.count50}
-Playcount: ${data.playcount}
-Ranked / Total Score: ${data.ranked_score} / ${data.total_score}
-\`\`\``;
+//                 return `\`\`\`
+// osu! profile information for: ${data.username}
+// ---------------------------------------------------------
+// Username: ${data.username}
+// User ID: ${data.user_id}
+// Avatar: https://a.ppy.sh/${data.user_id}.jpg
+// PP: ${Math.floor(Number(data.pp_raw))}pp
+// Rank: #${data.pp_rank} (#${data.pp_country_rank} in ${data.country})
+// Accuracy: ${Number(data.accuracy).toFixed(2)}%
+// Level: ${level} (${percentInLevel}%)
+// 300/100/50: ${data.count300}/${data.count100}/${data.count50}
+// Playcount: ${data.playcount}
+// Ranked / Total Score: ${data.ranked_score} / ${data.total_score}
+// \`\`\``;
+            
+                return {
+                    color: 12259703,
+                    url: `https://new.ppy.sh/u/${data.user_id}`,
+                    author: {
+                        name: `${data.username} - osu! profile data`,
+                        icon_url: "https://new.ppy.sh/images/layout/osu-logo.png"
+                    },
+                    thumbnail: {
+                        url: `https://a.ppy.sh/${data.user_id}`
+                    },
+                    fields: [
+                        {
+                            name: "User ID",
+                            value: data.user_id,
+                            inline: true
+                        },
+                        {
+                            name: "PP",
+                            value: `${Math.floor(Number(data.pp_raw))}pp`,
+                            inline: true
+                        },
+                        {
+                            name: "Rank (PP-based)",
+                            value: `#${data.pp_rank} (#${data.pp_country_rank} ${data.country})`,
+                            inline: true
+                        },
+                        {
+                            name: "Level",
+                            value: `${level} (${percentInLevel}%)`,
+                            inline: true
+                        },
+                        {
+                            name: "Accuracy",
+                            value: `${Number(data.accuracy).toFixed(2)}%`,
+                            inline: true
+                        },
+                        {
+                            name: "300/10/50 Count",
+                            value: `${data.count300}/${data.count100}/${data.count50}`,
+                            inline: true
+                        },
+                        {
+                            name: "Playcount",
+                            value: data.playcount,
+                            inline: true
+                        },
+                        {
+                            name: "Ranked Score",
+                            value: data.ranked_score,
+                            inline: true
+                        },
+                        {
+                            name: "Total Score",
+                            value: data.total_score,
+                            inline: true
+                        }
+                    ]
+                };
             });
     }
 }
